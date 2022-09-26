74772f8f60247413920505993c91b16825ae9a0c
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-09-26 19:11:13 +0300

    Spiral

--- Сolorful_hexagon_spiral.py
+++ Сolorful_hexagon_spiral.py
@@ -1,5 +1,5 @@
 from turtle import *
-color = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
 for i in range(360):
     pencolor(colors [x % 6])
     width(i / 100 + 1)
