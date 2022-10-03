f1523e958a954ef606ebdf29e5a4510cfc4ea0cb
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-10-03 08:34:47 +0300

    Ver. 0.2

--- All_in_one.py
+++ All_in_one.py
@@ -1,8 +1,8 @@
 from turtle import *
 
 command = input("What do you like to see?")
-match command.split
-    case[1]: #Simple square
+match command.split():
+    case["1"]: #Simple square
         forward(100)
         left(90)
         forward(100)
@@ -10,9 +10,9 @@ match command.split
         forward(100)
         left(90)
         forward(100)
-    case[2]: #Simple spiral
+    case["2"]: #Simple spiral
         for i in range(500): # this "for" repeat these function 500 times
-        forward(i)
-        left(91)
+            forward(i)
+            eft(91)
 
 
