3b68da6babe974457ccbec934dfeab655371c045
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-10-03 08:43:07 +0300

    Ver. 1.2

--- All_in_one.py
+++ All_in_one.py
@@ -1,5 +1,5 @@
 from turtle import *
-
+import random
 command = input("What do you like to see?")
 match command.split():
     case["1"]: #Simple square
@@ -10,9 +10,51 @@ match command.split():
         forward(100)
         left(90)
         forward(100)
+
     case["2"]: #Simple spiral
         for i in range(500): # this "for" repeat these function 500 times
             forward(i)
             eft(91)
 
+    case["3"]: #Colorful hexagon spiral
+        color = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+        for i in range(360):
+            pencolor(colors [x % 6])
+            width(i / 100 + 1)
+            forward(i)
+            left(59)
 
+    case["4"]: #A random blue flower
+        for n in range(60):
+            penup()
+            goto(random.randint(-400, 400), random.randint(-400, 400))
+            pendown()
+
+            red_amount   = random.randint( 0, 30) / 100.0
+            blue_amount  = random.randint(50,100) / 100.0
+            green_amount = random.randint( 0, 30) / 100.0
+
+            pencolor((red_amount, green_amount, blue_amount))
+
+            circle_size = random.randint(10, 40)
+            pensize(random.randint(1, 5))
+
+            for i in range(6):
+                circle(circle_size)
+                left(60)
+
+    case["5"]: #Random colorful flower
+        color = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+        for n in range(60):
+            pencolor(colors [n % 6])
+            width(n / 100 + 1)
+            forward(n)
+            left(59)
+            pencolor(colors [n % 6])
+
+            circle_size = random.randint(10, 40)
+            pensize(random.randint(1, 5))
+
+        for i in range(6):
+            circle(circle_size)
+            left(60)
