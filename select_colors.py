bc848a18aab05a3b4639c7ef54b2961857c412f5
Aleksandr <45598303+Horoshenkiyempty@users.noreply.github.com>
2022-10-04 17:59:11 +0300

    ver 1.2

--- select_colors.py
+++ select_colors.py
@@ -1,11 +1,13 @@
 select = []
-answer = input("Do you want change colors?  y - yes, n - no")
+answer = input("Do you want change colors?  y - yes, n - no    ")
+
 if answer == 'y':
     count = input("How manu colors?")
     answer = int(count)
-    for i in range(answer)
-         color = str input("Write a color " + i + " =")
+
+    for i in range(answer):
+         count = i
+         color = input("Write a color " + str(count + 1) + " =")
          select.append(color)
-else:
-    return
+
 print(select)
